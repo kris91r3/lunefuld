ff94908389da4eb4435d29c98b1fd8e1df966229
Kristina Andersen <kris91r3@stud.kea.dk>
2022-09-27 14:49:46 +0200

    cookie og hero

--- src/main.js
+++ src/main.js
@@ -1,1 +1,17 @@
 import "./style.css";
+
+const accepterCookie = document.querySelector("#accepter");
+const afvisCookie = document.querySelector("#afvis");
+const boks = document.querySelector("#cookieboks");
+
+accepterCookie.addEventListener("click", fjernBoks);
+afvisCookie.addEventListener("click", fjernBoks);
+
+function fjernBoks() {
+  boks.classList.add("hidden");
+  sessionStorage.setItem("fjernboks", true);
+}
+
+if (sessionStorage.getItem("fjernboks")) {
+  boks.classList.add("hidden");
+}
