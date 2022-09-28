63aaa851d9290e1ec36d4c6f5ba16be4e204912f
Kristina Andersen <kris91r3@stud.kea.dk>
2022-09-28 20:02:40 +0200

    nyt

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{html,vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        darkbeige: "#D3C8C5",
+        beige: "#ede9e8",
+        darkgreen: "#718355",
+        green: "#97a97c",
+        middlegreen: "#b5c99a",
+        lightgreen: "#cfe1b9",
+        easteryellow: "#FFC356",
+      },
+    },
   },
   plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
 };
